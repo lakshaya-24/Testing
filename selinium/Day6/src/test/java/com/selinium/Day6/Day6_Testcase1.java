9eac3b4f1081dc5a42d5e498bdb80f4fc5180892
lakshaya-24 <727821tuec115@skct.edu.in>
2023-06-06 22:48:55 +0530

    Day6

---
+++ selinium/Day6/src/test/java/com/selinium/Day6/Day6_Testcase1.java
@@ -0,0 +1,18 @@
+package com.selinium.Day6;
+
+public class Day6_Testcase1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		WebDriver wd=new ChromeDriver();
+		wd.get("https://demo.opencart.com/index.php?route=account/register&language=en-gb");
+		wd.findElement(By.xpath("//*[@id=\'input-firstname\']")).sendKeys("Student");
+		wd.findElement(By.xpath("//*[@id=\'input-lastname\']")).sendKeys("name");
+		wd.findElement(By.xpath("//*[@id=\'input-email\']")).sendKeys("Student@gmail.com");
+		wd.findElement(By.xpath("//*[@id=\'input-password\']")).sendKeys("Student12345");
+		wd.findElement(By.xpath("//*[@id=\'form-register\']/div/div/button")).click();		
+		
+
+	}
+}
